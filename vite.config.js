b54c5f50880b0ad4aec8af7cdccd8c4e21f12222
Raihanjansmaillendra <raihanjansmaillendra.rjs@gmail.com>
2025-04-21 06:58:32 +0700

    fix the https error

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/api/colormind": {
+        target: "http://colormind.io/api/",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/colormind/, ""),
+      },
+    },
+  },
 });
